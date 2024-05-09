5e0913f40eb2eb7dbe595e383ca528df128b5f0a
AHADI5 <gloireahadi9@gmail.com>
2024-05-09 18:02:36 +0200

    disigning option list

--- src/components/protected/class_room/section_tab.jsx
+++ src/components/protected/class_room/section_tab.jsx
@@ -1,72 +1,85 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import CustomModalAddOption from './add_section_modal';
 import ClassroomService from '../../../services/class_room_service';
-import { useParams } from 'react-router-dom';
-import { TailSpin } from 'react-loader-spinner';
-const SectionTab = ({ school }) => {
-  const [showModal, setShowModal] = useState(false);
-  const [classroomOptions, setClassroomOptions] = useState([]);
-  const [loading, setLoading] = useState(true);
-  
-  const params = useParams();
-  const { schoolID } = params;
+import {useParams} from 'react-router-dom';
+import {TailSpin} from 'react-loader-spinner';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCheckCircle} from '@fortawesome/free-regular-svg-icons';
+const SectionTab = ({school}) => {
+  const [showModal, setShowModal] = useState (false);
+  const [classroomOptions, setClassroomOptions] = useState ([]);
+  const [loading, setLoading] = useState (true);
 
-  useEffect(() => {
-    fetchClassroomOptions();
+  const params = useParams ();
+  const {schoolID} = params;
+
+  useEffect (() => {
+    fetchClassroomOptions ();
   }, []);
 
   const fetchClassroomOptions = async () => {
     try {
-      const options = await ClassroomService.getClassroomSection(schoolID);
-      setClassroomOptions(options);
-      console.log("Classroom options" , options)
-      setLoading(false);
+      const options = await ClassroomService.getClassroomSection (schoolID);
+      setClassroomOptions (options);
+      console.log ('Classroom options', options);
+      setLoading (false);
     } catch (error) {
-      console.error('Error fetching classroom options:', error);
-      setLoading(false);
+      console.error ('Error fetching classroom options:', error);
+      setLoading (false);
     }
   };
 
   const openModal = () => {
-    setShowModal(true);
+    setShowModal (true);
   };
 
   const closeModal = () => {
-    setShowModal(false);
+    setShowModal (false);
     // Refetch options when modal is closed to update the list if new options were added
-    fetchClassroomOptions();
+    fetchClassroomOptions ();
   };
 
   return (
     <div className="max-w-md mx-auto">
-      {loading ? (
-        <div className="flex items-center justify-center">
-        <TailSpin
-          visible={true}
-          height="30"
-          width="30"
-          color="rgb(255,255 ,255)"
-          ariaLabel="tail-spin-loading"
-          radius="0.5"
-          wrapperStyle={{}}
-          wrapperClass=""
-        />
-      </div>
-      ) : classroomOptions.length === 0 ? (
-        <div>
-          <p>No classroom options available. Please add new options.</p>
-          <button onClick={openModal}>Add New Option</button>
-          
-        </div>
-      ) : (
-        <div>
-          {/* Display existing options */}
-          {classroomOptions.map((option, index) => (
-            <div key={index}>{option.name}</div>
-          ))}
-          <button onClick={openModal}>Add New Option</button>
-        </div>
-      )}
+      {loading
+        ? <div className="flex items-center justify-center">
+            <TailSpin
+              visible={true}
+              height="30"
+              width="30"
+              color="rgb(255,255 ,255)"
+              ariaLabel="tail-spin-loading"
+              radius="0.5"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </div>
+        : classroomOptions.length === 0
+            ? <div>
+                <p>No classroom options available. Please add new options.</p>
+                <button
+                  onClick={openModal}
+                  className="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md transition duration-300 ease-in-out"
+                >
+                  Add New Option
+                </button>
+
+              </div>
+            : <div>
+                {/* Display existing options */}
+                {classroomOptions.map ((option, index) => (
+                  <div key={index} className="pb-4">
+                    <FontAwesomeIcon icon={faCheckCircle} /> {option.name}
+                  </div>
+                ))}
+                <button
+                  onClick={openModal}
+                  className="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md transition duration-300 ease-in-out"
+                >
+                  Add New Option
+                </button>
+
+              </div>}
       {showModal && <CustomModalAddOption onClose={closeModal} />}
     </div>
   );
