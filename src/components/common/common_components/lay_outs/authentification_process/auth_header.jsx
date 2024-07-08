31094a2624107d6c1f0d5b10f7aab9345d2f1dd2
AHADI5 <gloireahadi9@gmail.com>
2024-07-08 12:00:22 +0200

    login code UI refactoring

--- src/components/common/common_components/lay_outs/authentification_process/auth_header.jsx
+++ src/components/common/common_components/lay_outs/authentification_process/auth_header.jsx
@@ -1,26 +1,69 @@
 import ProfileDropdown from "../../other/user_role"
 import { useNavigate } from "react-router-dom"
 import logo from "../../../../../assets/img/ushirik-logo.svg"
+import Button from '@mui/material/Button';
+
 export default function AuthHeader(showLoginButton) {
-  const navigate = useNavigate();  
-  function redirectToLogin() {
+    const navigate = useNavigate();
+    function redirectToLogin() {
         navigate("/")
+    }
+    function redirectToCreateAccount() {
+        navigate("/signup")
     }
     return (
         <>
-          <div className="header-container flex justify-around gap-56">
-              <div className="header flex justify-center gap-2">
-                  <div className="logo flex"> <img src={logo} alt="Ushirik-educ" /></div>
-                  <div className="componey-name flex justify-center"> <h3>Ushirik </h3> </div>
-                 
-              </div>
-              {showLoginButton.shown === 0 && <div ><button onClick={redirectToLogin}>Login</button></div>}
-              {showLoginButton.shown === 1 && <div> <a href="">back to home page</a> </div>}
-              {showLoginButton.shown === 2 && <ProfileDropdown/>}
+            <div className="header-container flex justify-around gap-56">
+                <div className="header flex justify-center gap-2">
+                    <div className="logo flex"> <img src={logo} alt="Ushirik-educ" /></div>
+                    <div className="componey-name flex justify-center"> <h3>Ushirik </h3> </div>
 
-          </div>
-         
+                </div>
+                {showLoginButton.shown === 0 &&
+                    <div ><Button
+                        variant="contained"
+                        sx={{
+                            backgroundColor: '#1976d2', // Customize the background color
+                            color: '#ffffff', // Customize the text color
+                            textTransform: 'none', // Remove uppercase transformation
+                            fontSize: '13px', // Customize the font size
+                            padding: '5px 10px', // Customize the padding
+                            '&:hover': {
+                                backgroundColor: '#115293', // Customize the hover color
+                            },
+                        }}
+                        onClick={redirectToLogin}
+                    >
+                        Login
+                    </Button></div>
+                }
+                {showLoginButton.shown === 1 &&
+                    <div>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                backgroundColor: '#1976d2', // Customize the background color
+                                color: '#ffffff', // Customize the text color
+                                textTransform: 'none', // Remove uppercase transformation
+                                fontSize: '13px', // Customize the font size
+                                padding: '5px 10px', // Customize the padding
+                                '&:hover': {
+                                    backgroundColor: '#115293', // Customize the hover color
+                                },
+                            }}
+                            onClick={redirectToCreateAccount}
+                        >
+                            Créer un compte
+                        </Button>
+                    </div>
+                }
+                {showLoginButton.shown === 2 && <ProfileDropdown />}
+
+            </div>
+
         </>
+
+
     )
 
 }
--- src/components/common/common_components/lay_outs/login_layout.jsx
+++ src/components/common/common_components/lay_outs/login_layout.jsx
@@ -1,38 +1,23 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
-import Responsive from "./left_carroussel";
-import Carousel from "react-multi-carousel";
+import logo from "../../../../assets/img/app-img.png";
 
 export default function LoginRegisterLayout() {
   return (
     <>
       {/* Login header */}
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 p-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 h-screen relative">
         {/* Outlet */}
-        <div className="lg:col-span-2">
+        <div className="flex items-center justify-center p-4">
           <Outlet />
         </div>
 
-        {/* Carroussel */}
-        <div className="bg-gray-200">
-          {/* <Carousel  
-              responsive={Responsive}
-              swipeable={false}
-              draggable={false}
-              showDots={true}
-              autoPlay={true}
-              autoPlaySpeed={1000}
-              keyBoardControl={true}
-              centerMode={true}
-             
-          
-          >
-          <div className="w-full"><img src="https://picsum.photos/id/1/200/300" alt="" /></div>
-          <div><img src="https://picsum.photos/id/1/200/300" alt="" /></div>
-          <div><img src="https://picsum.photos/id/1/400/500" alt="" /></div>
-          <div><img src="https://picsum.photos/id/1/200/300" alt="" /></div>
-          <div><img src="https://picsum.photos/id/1/200/300" alt="" /></div>
-          <div><img src="https://picsum.photos/id/1/200/300" alt="" /></div>
-        </Carousel> */}
+        {/* Divider */}
+        <div className="absolute top-1/4 bottom-1/4 left-1/2 transform -translate-x-1/2 w-0.5 bg-gray-300 opacity-50"></div>
+
+        {/* Carousel */}
+        <div className="bg-white flex justify-center items-center p-4 ml-10">
+          <img src={logo} alt="app-image" className="max-w-full h-auto" />
         </div>
       </div>
       {/* Step process content */}
